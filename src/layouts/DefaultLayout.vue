9209e31310bad635d4f03c7ca243da005ac07ded
loicduong <mikhailcolt@gmail.com>
2025-08-01 09:32:10 +0700

    feat: add logout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -22,6 +22,9 @@
         <v-btn icon color="white" @click="showChangePassword = true" class="mt-4">
           <v-icon>mdi-lock-reset</v-icon>
         </v-btn>
+        <v-btn icon color="white" @click="handleLogout" class="mt-4">
+          <v-icon>mdi-logout</v-icon>
+        </v-btn>
       </div>
     </v-navigation-drawer>
 
@@ -38,10 +41,12 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 import { authHandler } from '@/services/authHandler'
 import ChangePasswordDialog from '@/components/ChangePasswordDialog.vue'
 import UpdateProfileDialog from '@/components/UpdateProfile.vue'
 
+const router = useRouter()
 const showChangePassword = ref(false)
 const showUpdateProfile = ref(false)
 const user = ref(null)
@@ -52,5 +57,10 @@ onMounted(() => {
 
 const onProfileUpdated = () => {
   user.value = authHandler.getDecodedToken()
+}
+
+const handleLogout = () => {
+  authHandler.logout()
+  router.push('/log-in')
 }
 </script>
